de77036e4304a9a77aad0f62de4757a4b262e3e1
Ivo Berger <ivo@ivoberger.com>
2019-10-04 21:52:55 +0200

    upgrades dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -7,80 +7,82 @@ import kotlin.String
  *   `$ ./gradlew buildSrcVersions`
  */
 object Libs {
-  /**
-   * https://developer.android.com/studio
-   */
-  const val aapt2: String = "com.android.tools.build:aapt2:" + Versions.aapt2
+    const val de_fayard_buildsrcversions_gradle_plugin: String =
+            "de.fayard.buildSrcVersions:de.fayard.buildSrcVersions.gradle.plugin:" +
+            Versions.de_fayard_buildsrcversions_gradle_plugin
 
-  /**
-   * https://developer.android.com/studio
-   */
-  const val com_android_tools_build_gradle: String = "com.android.tools.build:gradle:" +
-      Versions.com_android_tools_build_gradle
+    /**
+     * https://developer.android.com/studio
+     */
+    const val com_android_tools_build_gradle: String = "com.android.tools.build:gradle:" +
+            Versions.com_android_tools_build_gradle
 
-  /**
-   * https://developer.android.com/studio
-   */
-  const val lint_gradle: String = "com.android.tools.lint:lint-gradle:" + Versions.lint_gradle
+    /**
+     * http://www.github.com/kotlintest/kotlintest
+     */
+    const val kotlintest_runner_junit5: String = "io.kotlintest:kotlintest-runner-junit5:" +
+            Versions.kotlintest_runner_junit5
 
-  const val jacoco_android: String = "com.dicedmelon.gradle:jacoco-android:" +
-      Versions.jacoco_android
+    const val play_services_location: String = "com.google.android.gms:play-services-location:" +
+            Versions.com_google_android_gms
 
-  const val play_services_location: String = "com.google.android.gms:play-services-location:" +
-      Versions.com_google_android_gms
+    const val play_services_maps: String = "com.google.android.gms:play-services-maps:" +
+            Versions.com_google_android_gms
 
-  const val play_services_maps: String = "com.google.android.gms:play-services-maps:" +
-      Versions.com_google_android_gms
+    /**
+     * https://github.com/wupdigital/android-maven-publish
+     */
+    const val android_maven_publish: String = "digital.wup:android-maven-publish:" +
+            Versions.android_maven_publish
 
-  const val de_fayard_buildsrcversions_gradle_plugin: String =
-      "de.fayard.buildSrcVersions:de.fayard.buildSrcVersions.gradle.plugin:" +
-      Versions.de_fayard_buildsrcversions_gradle_plugin
+    /**
+     * https://kotlinlang.org/
+     */
+    const val kotlin_android_extensions: String =
+            "org.jetbrains.kotlin:kotlin-android-extensions:" + Versions.org_jetbrains_kotlin
 
-  /**
-   * https://github.com/wupdigital/android-maven-publish
-   */
-  const val android_maven_publish: String = "digital.wup:android-maven-publish:" +
-      Versions.android_maven_publish
+    /**
+     * https://kotlinlang.org/
+     */
+    const val kotlin_android_extensions_runtime: String =
+            "org.jetbrains.kotlin:kotlin-android-extensions-runtime:" +
+            Versions.org_jetbrains_kotlin
 
-  /**
-   * http://www.github.com/kotlintest/kotlintest
-   */
-  const val kotlintest_runner_junit5: String = "io.kotlintest:kotlintest-runner-junit5:" +
-      Versions.kotlintest_runner_junit5
+    /**
+     * https://kotlinlang.org/
+     */
+    const val kotlin_gradle_plugin: String = "org.jetbrains.kotlin:kotlin-gradle-plugin:" +
+            Versions.org_jetbrains_kotlin
 
-  const val dokka_android_gradle_plugin: String =
-      "org.jetbrains.dokka:dokka-android-gradle-plugin:" + Versions.org_jetbrains_dokka
+    /**
+     * https://kotlinlang.org/
+     */
+    const val kotlin_scripting_compiler_embeddable: String =
+            "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:" +
+            Versions.org_jetbrains_kotlin
 
-  const val dokka_gradle_plugin: String = "org.jetbrains.dokka:dokka-gradle-plugin:" +
-      Versions.org_jetbrains_dokka
+    /**
+     * https://kotlinlang.org/
+     */
+    const val kotlin_stdlib_jdk8: String = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:" +
+            Versions.org_jetbrains_kotlin
 
-  /**
-   * https://kotlinlang.org/
-   */
-  const val kotlin_android_extensions_runtime: String =
-      "org.jetbrains.kotlin:kotlin-android-extensions-runtime:" + Versions.org_jetbrains_kotlin
+    const val dokka_android_gradle_plugin: String =
+            "org.jetbrains.dokka:dokka-android-gradle-plugin:" + Versions.org_jetbrains_dokka
 
-  /**
-   * https://kotlinlang.org/
-   */
-  const val kotlin_android_extensions: String = "org.jetbrains.kotlin:kotlin-android-extensions:" +
-      Versions.org_jetbrains_kotlin
+    const val dokka_gradle_plugin: String = "org.jetbrains.dokka:dokka-gradle-plugin:" +
+            Versions.org_jetbrains_dokka
 
-  /**
-   * https://kotlinlang.org/
-   */
-  const val kotlin_gradle_plugin: String = "org.jetbrains.kotlin:kotlin-gradle-plugin:" +
-      Versions.org_jetbrains_kotlin
+    const val jacoco_android: String = "com.dicedmelon.gradle:jacoco-android:" +
+            Versions.jacoco_android
 
-  /**
-   * https://kotlinlang.org/
-   */
-  const val kotlin_scripting_compiler_embeddable: String =
-      "org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:" + Versions.org_jetbrains_kotlin
+    /**
+     * https://developer.android.com/studio
+     */
+    const val lint_gradle: String = "com.android.tools.lint:lint-gradle:" + Versions.lint_gradle
 
-  /**
-   * https://kotlinlang.org/
-   */
-  const val kotlin_stdlib_jdk8: String = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:" +
-      Versions.org_jetbrains_kotlin
+    /**
+     * https://developer.android.com/studio
+     */
+    const val aapt2: String = "com.android.tools.build:aapt2:" + Versions.aapt2
 }
